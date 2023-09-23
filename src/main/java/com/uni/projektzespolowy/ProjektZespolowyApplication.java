a61a20e5f9aa3d8f37e064c303962359c86e5c3e
adyrdul <ania16405@gmail.com>
2023-09-23 19:58:27 +0200

    init commit

---
+++ src/main/java/com/uni/projektzespolowy/ProjektZespolowyApplication.java
@@ -0,0 +1,13 @@
+package com.uni.projektzespolowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektZespolowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektZespolowyApplication.class, args);
+	}
+
+}
